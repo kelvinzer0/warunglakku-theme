cf7fa249c0508b909171778f5b1468bcbb1d613e
Z User <z@container>
2026-07-15 12:10:31 +0000

    feat(contactus): update with real Warung Lakku address + info
    Replaced Odoo default fake data on /contactus page:
      OLD: 'My Company, 3575 Fake Buena Vista Avenue, +1 555-555-5556,
            info@yourcompany.example.com'
      NEW: 'Warung Lakku, Dusun Ngrekesan Desa Ngadri Kec. Binangun
            Kab. Blitar 66193, +62 857-3355-6953 (WhatsApp),
            hello@warunglakku.com'
    
    Also added:
      - Jam Operasional: Senin-Minggu 10:00-22:00 WIB
      - WhatsApp link (wa.me/6285733556953)
      - Email link (mailto:hello@warunglakku.com)
      - Page title + intro text in Indonesian
    
    Company record (res.company id=1) updated via RPC:
      - email: hello@warunglakku.com
      - phone: +62 857-3355-6953
      - street/street2/city/zip: full Blitar address
    
    Bump version: 17.0.3.10.62 → 17.0.3.10.63

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Warung Lakku Theme',
-    'version': '17.0.3.10.62',
+    'version': '17.0.3.10.63',
     'depends': ['website', 'website_sale', 'website_sale_pickup_at_store'],
     'data': [
         'views/assets.xml',
@@ -18,6 +18,7 @@
         'views/pickup_mode_redesign.xml',
         'views/payment_delivery_redesign.xml',
         'views/product_terms_cleanup.xml',
+        'views/contactus_page.xml',
     ],
     'license': 'LGPL-3',
 }
